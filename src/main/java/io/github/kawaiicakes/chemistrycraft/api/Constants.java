61902e905e91887603f4fe37ad7cef065c9afe74
kawaiicakes <vomitmopper@gmail.com>
2023-08-17 07:26:36 -0600

    Renamed, moved, and changed from enum class describing thermodynamic constants to a class with static fields describing any useful scientific constant

---
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/Constants.java
@@ -0,0 +1,11 @@
+package io.github.kawaiicakes.chemistrycraft.api;
+
+/**
+ * Avoids magic numbers. Measured in Celsius.
+ */
+public class Constants {
+    public static final float LAVA_TEMPERATURE = (1000.00F);
+    public static final float FREEZING_POINT_WATER = 0.00F;
+    public static final float BOILING_POINT_WATER = 100.00F;
+    public static final float ABSOLUTE_ZERO = -273.15F;
+}
