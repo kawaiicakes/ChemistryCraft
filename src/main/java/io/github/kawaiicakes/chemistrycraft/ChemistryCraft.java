a8fb03ca107b363dfbc488c061224befd40e8376
kawaiicakes <vomitmopper@gmail.com>
2023-08-17 07:24:02 -0600

    Added TODO

--- src/main/java/io/github/kawaiicakes/chemistrycraft/ChemistryCraft.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/ChemistryCraft.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 
 // TODO: tooltips can be modified through events (RenderTooltipEvent.GatherComponents)
 // TODO: research materials science https://www.quora.com/How-do-you-increase-the-tensile-strength-of-a-sword-and-or-blade , https://www.google.com/search?q=what+kind+of+stress+is+applied+to+a+sword+blade&sca_esv=557314902&rlz=1C1VDKB_enCA1069CA1069&sxsrf=AB5stBjIWTS4cZCoqDP-Y_Kr20CrMhqn4w%3A1692152470603&ei=ljLcZKK4JMuO0PEP9tu1oAg&ved=0ahUKEwii26mXj-CAAxVLBzQIHfZtDYQQ4dUDCBA&uact=5&oq=what+kind+of+stress+is+applied+to+a+sword+blade&gs_lp=Egxnd3Mtd2l6LXNlcnAiL3doYXQga2luZCBvZiBzdHJlc3MgaXMgYXBwbGllZCB0byBhIHN3b3JkIGJsYWRlMgcQIRigARgKSLw0UABYgzRwAngBkAEBmAGLAaAB4ySqAQUzNy4xM7gBA8gBAPgBAcICBxAjGIoFGCfCAgQQIxgnwgIHEAAYigUYQ8ICDRAAGIoFGLEDGIMBGEPCAggQABiABBixA8ICCBAAGIoFGLEDwgIFEAAYgATCAgYQABgWGB7CAggQABgWGB4YD8ICBxAAGA0YgATCAgoQABiABBgUGIcCwgIIEAAYFhgeGArCAggQABiKBRiGA8ICBRAhGKAB4gMEGAAgQYgGAQ&sclient=gws-wiz-serp
+// TODO: look into PortalShape or the pathfinding method for making multiblock structures
 
 @Mod(ChemistryCraft.MOD_ID)
 public class ChemistryCraft
