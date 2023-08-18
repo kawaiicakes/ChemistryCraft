1bcde958a759bde0cfdf9d76126b409969900bbe
kawaiicakes <vomitmopper@gmail.com>
2023-08-18 03:28:21 -0600

    Added field for enthalpy of formation reference states.

--- src/main/java/io/github/kawaiicakes/chemistrycraft/api/Constants.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/Constants.java
@@ -1,11 +1,10 @@
 package io.github.kawaiicakes.chemistrycraft.api;
 
 /**
- * Avoids magic numbers. Measured in Celsius.
+ * Avoids magic numbers. Measured in Celsius and other standard SI units where appropriate.
  */
 public class Constants {
+    public static final byte STANDARD_ENTHALPY = 0;
     public static final float LAVA_TEMPERATURE = (1000.00F);
-    public static final float FREEZING_POINT_WATER = 0.00F;
-    public static final float BOILING_POINT_WATER = 100.00F;
     public static final float ABSOLUTE_ZERO = -273.15F;
 }
