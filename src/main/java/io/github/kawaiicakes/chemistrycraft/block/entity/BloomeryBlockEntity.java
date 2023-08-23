6bfda94c43111c5b01e1fef31b32fc81a26aaf94
kawaiicakes <vomitmopper@gmail.com>
2023-08-23 10:52:49 -0600

    Removed progress reset upon running out of energy

--- src/main/java/io/github/kawaiicakes/chemistrycraft/block/entity/BloomeryBlockEntity.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/block/entity/BloomeryBlockEntity.java
@@ -221,7 +221,6 @@ public class BloomeryBlockEntity extends BlockEntity implements MenuProvider {
                     craftItem(bloomeryBlockEntity);
                 }
             } else {
-                bloomeryBlockEntity.resetProgress();
                 setChanged(level, blockPos, blockState);
             }
         }
