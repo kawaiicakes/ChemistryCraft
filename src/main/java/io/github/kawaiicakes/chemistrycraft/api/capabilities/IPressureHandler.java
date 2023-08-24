b4680e5aec8fbae3cb29e4ec05caced35397028d
kawaiicakes <vomitmopper@gmail.com>
2023-08-24 15:31:42 -0600

    Now considers volume

--- src/main/java/io/github/kawaiicakes/chemistrycraft/api/capabilities/IPressureHandler.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/capabilities/IPressureHandler.java
@@ -7,6 +7,11 @@ import net.minecraftforge.common.capabilities.AutoRegisterCapability;
  */
 @AutoRegisterCapability
 public interface IPressureHandler {
+    /**
+     * Return the container volume.
+     * @return The float representing container volume in meters cubed.
+     */
+    float getContainerVolume();
     void increasePressure(float kPa);
     void decreasePressure(float kPa);
     float getCurrentPressure();
