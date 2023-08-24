a5eee5577294b5f6092f9adea88cb7a71b671acb
kawaiicakes <vomitmopper@gmail.com>
2023-08-24 15:02:15 -0600

    Forgor to actually register the capability :skull:

--- src/main/java/io/github/kawaiicakes/chemistrycraft/api/capabilities/IPressureHandler.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/capabilities/IPressureHandler.java
@@ -1,8 +1,11 @@
 package io.github.kawaiicakes.chemistrycraft.api.capabilities;
 
+import net.minecraftforge.common.capabilities.AutoRegisterCapability;
+
 /**
  * This capability is the unit of interaction for pressure inventories. Assume floats are measured in kPa.
  */
+@AutoRegisterCapability
 public interface IPressureHandler {
     void increasePressure(float kPa);
     void decreasePressure(float kPa);
