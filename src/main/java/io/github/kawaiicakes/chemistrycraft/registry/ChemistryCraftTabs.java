204402d63c1fd42373c8898144607e508fa4d18f
kawaiicakes <vomitmopper@gmail.com>
2023-10-04 00:28:19 -0600

    Self-explanatory function.

---
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/registry/ChemistryCraftTabs.java
@@ -0,0 +1,29 @@
+package io.github.kawaiicakes.chemistrycraft.registry;
+
+import com.smashingmods.chemlib.registry.ItemRegistry;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ChemistryCraftTabs {
+    public static final CreativeModeTab ORES = new CreativeModeTab("chemistrycraft_ores") {
+        @Override
+        public ItemStack makeIcon() {
+            return ItemRegistry.getRegistryObject(ItemRegistry.REGISTRY_MISC_ITEMS, "periodic_table").get()
+                    .getDefaultInstance();
+        }
+    };
+    public static final CreativeModeTab MINERALS = new CreativeModeTab("chemistrycraft_minerals") {
+        @Override
+        public ItemStack makeIcon() {
+            return ItemRegistry.getRegistryObject(ItemRegistry.REGISTRY_MISC_ITEMS, "periodic_table").get()
+                    .getDefaultInstance();
+        }
+    };
+    public static final CreativeModeTab BLOCKS = new CreativeModeTab("chemistrycraft_blocks") {
+        @Override
+        public ItemStack makeIcon() {
+            return ItemRegistry.getRegistryObject(ItemRegistry.REGISTRY_MISC_ITEMS, "periodic_table").get()
+                    .getDefaultInstance();
+        }
+    };
+}
