e33a3add639fe7a5bc8a407744f15b4e373a0c13
kawaiicakes <vomitmopper@gmail.com>
2023-10-05 15:54:00 -0600

    package info

--- src/main/java/io/github/kawaiicakes/chemistrycraft/api/package-info.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/package-info.java
@@ -1,1 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 package io.github.kawaiicakes.chemistrycraft.api;
+
+import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
