c1b187477317fa09f5997e9efe09a881063a276e
kawaiicakes <vomitmopper@gmail.com>
2023-10-05 20:02:42 -0600

    beginning of automatic registration of items based on json files

--- src/main/java/io/github/kawaiicakes/chemistrycraft/registry/ChemistryCraftRegistry.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/registry/ChemistryCraftRegistry.java
@@ -2,7 +2,13 @@ package io.github.kawaiicakes.chemistrycraft.registry;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.smashingmods.chemlib.common.items.ElementItem;
+import io.github.kawaiicakes.chemistrycraft.registry.item.AllotropeMineralItem;
+import net.minecraft.world.item.Item;
 
+import java.util.Optional;
+
+import static com.smashingmods.chemlib.registry.ItemRegistry.getElementByName;
 import static com.smashingmods.chemlib.registry.Registry.getStreamAsJsonObject;
 
 public class ChemistryCraftRegistry {
@@ -10,7 +16,13 @@ public class ChemistryCraftRegistry {
 
     public static void registerMinerals() {
         for (JsonElement jsonElement: MINERALS.getAsJsonArray("allotropes")) {
+            JsonObject obj = jsonElement.getAsJsonObject();
+            String name = obj.get("name").getAsString();
+            String phase = obj.get("phase").getAsString().isEmpty() ? null : obj.get("phase").getAsString();
+            String description = obj.get("description").getAsString();
+            String parent = obj.get("parent").getAsString();
 
+            ItemRegistry.registerOre(new AllotropeMineralItem(name, phase, description, parent));
         }
 
         for (JsonElement jsonElement : MINERALS.getAsJsonArray("non_allotropes")) {
