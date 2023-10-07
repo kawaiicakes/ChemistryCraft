18e2ad2ebad4ba8726275c72d86ac5dd883bdf2d
kawaiicakes <vomitmopper@gmail.com>
2023-10-07 00:50:13 -0600

    - Committing only so the new datagen package is visible. Might not even stick around

---
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/init/datagen/ChemistryCraftLootTableProvider.java
@@ -0,0 +1,10 @@
+package io.github.kawaiicakes.chemistrycraft.init.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.loot.LootTableProvider;
+
+public class ChemistryCraftLootTableProvider extends LootTableProvider {
+    public ChemistryCraftLootTableProvider(DataGenerator pGenerator) {
+        super(pGenerator);
+    }
+}
