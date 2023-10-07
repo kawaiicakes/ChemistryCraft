ce42ff1641c03002ff85bd5cf2e4e02d48512486
kawaiicakes <vomitmopper@gmail.com>
2023-10-06 23:48:49 -0600

    - Now explicitly overrides methods in Chemical such that implementing types are reminded to override these even if superclasses of that type already override them - #getPhase should not be nullable. - Added annotations to #getMatterState

--- src/main/java/io/github/kawaiicakes/chemistrycraft/api/Mineral.java
+++ src/main/java/io/github/kawaiicakes/chemistrycraft/api/Mineral.java
@@ -2,13 +2,18 @@ package io.github.kawaiicakes.chemistrycraft.api;
 
 import com.smashingmods.chemlib.api.Chemical;
 import com.smashingmods.chemlib.api.MatterState;
-import org.jetbrains.annotations.Nullable;
 
 public interface Mineral extends Chemical {
-    @Nullable
+    @Override
+    String getChemicalName();
+    @Override
+    String getChemicalDescription();
+    @Override
+    String getAbbreviation();
     String getPhase();
     String getParentChemical();
 
+    @Override
     default MatterState getMatterState() {
         return MatterState.SOLID;
     }
